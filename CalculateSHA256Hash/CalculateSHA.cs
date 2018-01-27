ef5d58da7d6d18c2d6faf784d980bcf0b1dd76ec
Sevgin Mustafov <sevgin95@gmail.com>
2018-01-27 18:45:47 +0200

    Initial Commit

---
+++ CalculateSHA256Hash/CalculateSHA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CalculateSHA256Hash
+{
+    class CalculateSHA
+    {
+        public static void Main(string[] args)
+        {
+            string result = getHashSha256("Hello_World");
+
+            Console.WriteLine(result);
+        }
+
+        public static string getHashSha256(string text)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(text);
+
+            SHA256Managed hashstring = new SHA256Managed();
+
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = null;
+
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+
+            return hashString;
+        }
+    }
+}
